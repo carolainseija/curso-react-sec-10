78fb87d6d5e7dd90cbc8bc9323bcf306e4462476
carolainseija <carolainsilva1@gmail.com>
2022-03-22 10:40:35 -0300

    app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect  } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,8 +6,14 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-  
+
+  useEffect (()=> {
+    const storageUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+    if (storageUserLoggedInInformation === "1") {
+      setIsLoggedIn(true)
+    }
+  }, []) 
+
   const loginHandler = (email, password) => {
     localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
@@ -16,14 +22,6 @@ function App() {
   const logoutHandler = () => {
     setIsLoggedIn(false);
   };
-
-  useEffect = (()=> {
-    const storageUserLoggedInInformation = localStorage.setItem('isLoggedIn')
-    if (storageUserLoggedInInformation === "1") {
-      setIsLoggedIn(true)
-    }
-  
-  }, [])
 
 
   return (
